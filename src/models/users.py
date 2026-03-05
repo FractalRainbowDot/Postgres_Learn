3c39f6cea5ad35416431590e22bce5464433b1df
VadosLAPTOP <pinkpocket.cream@gmail.com>
2026-03-05 10:37:58 +0300

    DB gender -> Enum

--- src/models/users.py
+++ src/models/users.py
@@ -1,7 +1,8 @@
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.models.base import Base
+from src.schemas.users import Gender
 
 
 class UserModel(Base):
@@ -11,7 +12,7 @@ class UserModel(Base):
     name: Mapped[str] = mapped_column(String(20), nullable=False, )
     surname: Mapped[str] = mapped_column(String(50), nullable=False, )
     age: Mapped[int] = mapped_column(Integer, nullable=False, )
-    gender: Mapped[str] = mapped_column(String(6), nullable=False, )
+    gender: Mapped[Gender] = mapped_column(Enum(Gender), nullable=False, )
     email: Mapped[str] = mapped_column(String(50), nullable=False, )
     password: Mapped[str] = mapped_column(String(50), nullable=False, )
     is_active: Mapped[bool] = mapped_column(default=True,)
